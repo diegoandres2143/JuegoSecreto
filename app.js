2e8b2ac4fe3d31292813620f97228227e5fb28b0
Diego Andrés Candamil Puerta <diegoandres2143@gmail.com>
2024-07-07 22:03:19 -0600

    Update app.js
    Agradecimiento final

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ function generarNumeroSecreto() {
     console.log(listaNumerosSorteados);
     //Si ya sorteamos todos los números
     if (listaNumerosSorteados.length == numeroMaximo) {
-        asignarTextoElemento('p','Ya se sortearon todos los números posibles, ¡gracias por jugar mi juego!');
+        asignarTextoElemento('p','Has acertado todos los números posibles, ¡gracias por jugar!');
     } else {
         //Si el numero generado está incluido en la lista 
         if (listaNumerosSorteados.includes(numeroGenerado)) {
