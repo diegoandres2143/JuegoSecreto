fdc5e307248a151f987c654c942cf92ffede9a58
Diego Andrés Candamil Puerta <diegoandres2143@gmail.com>
2024-07-07 21:44:45 -0600

    Update app.js
    Indica -> Escribe

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ function generarNumeroSecreto() {
 
 function condicionesIniciales() {
     asignarTextoElemento('h1','Juego del número secreto!');
-    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
+    asignarTextoElemento('p',`Escribe un número del 1 al ${numeroMaximo}`);
     numeroSecreto = generarNumeroSecreto();
     intentos = 1;
     console.log(numeroSecreto);
